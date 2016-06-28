4d53e6f144a128512d9860ed0a2040e78aa88411
tefitacosta <tefitacosta@gmail.com>
2016-06-28 17:34:39 -0500

    Con OperationClass hecha

---
+++ operation_class.rb
@@ -0,0 +1,24 @@
+#En este archivo hago la primera clase de Ruby
+class OperationClass
+
+  def self.sume_us (element1, element2)
+    puts "#{element1 + element2}"
+  end
+
+  def self.substract_us(element1, element2)
+    puts "#{element1 - element2}"
+  end
+
+  def self.divide_us(element1, element2)
+    puts "#{element1.to_f / element2}"
+  end
+
+  def self.multi_us (element1, element2)
+    puts "#{element1 * element2}"
+  end
+
+  def self.string_us (element1, element2)
+    puts "#{element1}#{element2}"
+  end
+
+end
